831678fad863bd9d3ee0d2f92551fa4f0b488172
Aditya Tomar <adityatomar80050@gmail.com>
2025-05-09 03:47:24 +0530

    Moved from server side rendering to client side rendering for projects

--- src/app/(authenticated)/project/[id]/page.js
+++ src/app/(authenticated)/project/[id]/page.js
@@ -1,6 +1,5 @@
-'use server'
+'use client'
 
-import { cookies } from 'next/headers';
 import Image from "next/image";
 
 
@@ -10,7 +9,8 @@ import { nunito_sans } from "@/app/fonts/nunito_sans";
 
 
 import ProjectFeedbackContainer from '@/app/components/feedback/project_feedback_container';
-import { permanentRedirect } from 'next/navigation';
+import { useGetData } from '@/app/customHooks/useGetData';
+import { useParams } from "next/navigation";
 
 
 
@@ -20,42 +20,22 @@ import { permanentRedirect } from 'next/navigation';
 
 
 
-export default async function Project({params}){
+export default function Project(){
+
+    const params = useParams();
+
+    const { loading , data , error } = useGetData(`projects/${params.id}`)
 
 
-    const cookieStore = await cookies();
-    const cookieHeader = cookieStore.toString();
+    if(!loading && !error){
 
-    const { id } = await params;
+        const date = new Date(parseFloat(data?.date ? data.date : 0));
 
-    const response = await fetch(`${process.env.REMOTE_URL}/projects/${id}` , {
-        method : 'get',
-        credentials : 'include',
-        headers : {
-            Cookie : cookieHeader
-        }
-    });
+        const dateString = `${date?.getDate()}/${date?.getMonth()}/${date?.getFullYear()}`;
 
 
-    if(!response.ok){
-
-        if(response.status === 401){
-
-            permanentRedirect('/auth/signin');
-        }
-
-        return <span>Error Occured</span>
-    }
-
-    const data = await response.json();
-
-    const date = new Date(parseFloat(data[0].date));
-
-    const dateString = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-
-    return(
-
-        <div className='grid min-[800px]:grid-cols-2 min-[1000px]:grid-cols-[60%_40%] min-[800px]:pb-10'>
+        return(
+            <div className='grid min-[800px]:grid-cols-2 min-[1000px]:grid-cols-[60%_40%] min-[800px]:pb-10'>
 
 
             {/* PROJECT DETAILS */}
@@ -68,14 +48,14 @@ export default async function Project({params}){
                             height={30}
                             alt='User Image'
                         />
-                        <span className={`${nunito_sans.className} font-[700] text-black`}>{data[0].username}</span>
-                        <span className={`${nunito_sans.className} font-[500] ${data[0].status === 'complete' ? 'bg-green-500' : 'bg-purple-400'} self-start text-[12px] text-white bg-green px-3 py-1 rounded-full`}>{data[0].status?.toUpperCase()}</span>
+                        <span className={`${nunito_sans.className} font-[700] text-black`}>{data[0]?.username}</span>
+                        <span className={`${nunito_sans.className} font-[500] ${data[0]?.status === 'complete' ? 'bg-green-500' : 'bg-purple-400'} self-start text-[12px] text-white bg-green px-3 py-1 rounded-full`}>{data[0]?.status?.toUpperCase()}</span>
                     </div>
                     <h1 className={`${nunito_sans.className} font-[700] text-black text-2xl`}>Project Title</h1>
-                    <p className={`${nunito_sans.className} font-[500] text-gray-800`}>{data[0].content}</p>
+                    <p className={`${nunito_sans.className} font-[500] text-gray-800`}>{data[0]?.content}</p>
 
                     <div className='grid grid-cols-[1fr_max-content] items-center'>
-                        <a href={data[0].link} target='_blank' className={`place-self-start ${nunito_sans.className} font-[600] bg-[#0391da] text-white hover:bg-white hover:text-[#0391da] border-3 border-[#0391da] px-3 py-1 rounded text-[15px] transition-color duration-300 ease-in-out`}>View Site</a>
+                        <a href={data[0]?.link} target='_blank' className={`place-self-start ${nunito_sans.className} font-[600] bg-[#0391da] text-white hover:bg-white hover:text-[#0391da] border-3 border-[#0391da] px-3 py-1 rounded text-[15px] transition-color duration-300 ease-in-out`}>View Site</a>
                         <span className={`text-gray-500 ${nunito_sans.className} text-[13px] font-[500]`}>{dateString}</span>
                     </div>
                 </div>
@@ -84,10 +64,21 @@ export default async function Project({params}){
 
 
             {/* FEEDBACKS */}
-            <ProjectFeedbackContainer id={id} />
+            <ProjectFeedbackContainer id={params.id} />
             {/* FEEDBACKS */}
 
-        </div>
+            </div>
+        )
+    }
 
-    );
+    if(error && !error.ok){
+
+        if(error.status === 401){
+
+            window.location.replace('/auth/signin');
+        }
+
+        return <span>Error Occured</span>
+    }
+
 }
